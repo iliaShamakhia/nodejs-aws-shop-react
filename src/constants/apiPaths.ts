5eaf9a83fcda918b5f4322b1c5fe7e344d182c8a
iliaShamakhia <shamakhiailia@gmail.com>
2025-04-13 20:15:56 +0400

    add load balancer endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://l5n7gzor31.execute-api.eu-north-1.amazonaws.com/prod",
-  order: "http://iliashamakhia-cart-api-dev.eu-north-1.elasticbeanstalk.com/api/profile/cart",
+  order: "https://cart-api-load-balancer-1551832212.eu-north-1.elb.amazonaws.com/api/profile/cart",
   import: "https://9pum4sevo7.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "http://iliashamakhia-cart-api-dev.eu-north-1.elasticbeanstalk.com/api",
+  cart: "https://cart-api-load-balancer-1551832212.eu-north-1.elb.amazonaws.com/api",
 };
 
 export default API_PATHS;
