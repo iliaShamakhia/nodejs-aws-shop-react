2348be76b6a524662d7658db8bfffe3468da020a
iliaShamakhia <shamakhiailia@gmail.com>
2025-04-14 19:54:28 +0400

    update endpoints

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://l5n7gzor31.execute-api.eu-north-1.amazonaws.com/prod",
-  order: "https://cart-api-load-balancer-1551832212.eu-north-1.elb.amazonaws.com/api/profile/cart",
+  order: "https://dhy3ulgn56rig.cloudfront.net/api/profile/cart",
   import: "https://9pum4sevo7.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://cart-api-load-balancer-1551832212.eu-north-1.elb.amazonaws.com/api",
+  cart: "https://dhy3ulgn56rig.cloudfront.net/api",
 };
 
 export default API_PATHS;
